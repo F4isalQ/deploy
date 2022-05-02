47f788e9577d4c03ccc775104b5566afeb24f750
Faisal Alqahtani <89203484+F4isalQ@users.noreply.github.com>
2022-05-02 03:38:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify
 import pickle
 from resemblyzer import VoiceEncoder, preprocess_wav
 import warnings
-from pydub import AudioSegment
 import torch
 from ann import ANN
 import soundfile as sf
@@ -14,7 +13,7 @@ warnings.filterwarnings("ignore")
 
 #model=torch.load('/Users/saudshranr/Downloads/ann-100q-97.6.pth', map_location="cpu")
 model = ANN(input_dim=256, output_dim=100)
-model.load_state_dict(torch.load('/Users/saudshranr/Downloads/ann-100q-97.6.pth', map_location=torch.device('cpu')))
+model.load_state_dict(torch.load('/Users/fasilsaeeud/Desktop/deploy_new/ann-100q-97.6.pth', map_location=torch.device('cpu')))
 
 
 app = Flask(__name__)
@@ -32,7 +31,7 @@ def extract(fname):
     signal = encoder.embed_utterance(torch.tensor(signal))
     return signal
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/pre',methods=['GET','POST'])
 def predict():
 
     logdata = request.stream.read()
@@ -40,9 +39,7 @@ def predict():
     with open('try1.mp3', 'wb') as f:
         f.write(logdata)
 
-    mp3_sound = AudioSegment.from_file('try1.mp3')
-    #rename them using the old name + ".wav"
-    mp3_sound.export('try1.wav', format="wav")
+
 
     f = extract('try1.wav')
     model.eval()
@@ -51,7 +48,7 @@ def predict():
 
 
     top_p, top_class = prediction.topk(1, dim=1)
-    df=pd.read_csv('/Volumes/TOSHIBA/readers_100.csv')
+    df=pd.read_csv('/Users/fasilsaeeud/Desktop/deploy_new/readers_100.csv')
     id=top_p.item()
     data = df[df['class_id'] == id]
     name = data['qari_ar']
@@ -61,10 +58,9 @@ def predict():
 
 
 
-@app.route('/ex',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def hello():
     return 'hhhhhh'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True,port=5000)
+
 
